0745983201982e17294e6f9efd17c33eb9a95edf
nickart98 <nikita.artemov.9870@gmail.com>
2020-08-31 02:16:16 +0700

    Upd for day7

---
+++ src/main/java/day7/Task2/Player.java
@@ -0,0 +1,49 @@
+package day7.Task2;
+
+
+import java.util.Random;
+
+public class Player {
+    private static int stamina, countPlayers;
+    public final static int MAX_STAMINA = 100, MIN_STAMINA = 0;
+    static int i;
+    private static int[] players = new int[6];
+    
+
+    public static void counter() {
+        Random rd = new Random();
+        for (int i = 0; i < players.length; i++) {
+            stamina = rd.nextInt(10) + 90;
+            countPlayers++;
+        }
+        System.out.println("В игру вошло " + countPlayers + " игроков");
+    }
+
+    public static void run() {
+        while (stamina > MIN_STAMINA) {
+            stamina--;
+        }
+        System.out.println("Из игры выбыл 1 игрок");
+        countPlayers--;
+    }
+
+    public static void info() {
+        switch (countPlayers) {
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("Команды неполные, есть еще " + (6 - countPlayers) + " свободных места");
+                break;
+            case 1:
+            case 0:
+                System.out.println("Команды неполные, есть еще " + (6 - countPlayers) + " свободных мест");
+                break;
+            case 5:
+                System.out.println("Команды неполные, есть еще " + (6 - countPlayers) + " свободное место");
+                break;
+            default:
+                System.out.println("Команды полные");
+                break;
+        }
+    }
+}
---
+++ src/main/java/day7/Task2/Task2.java
@@ -0,0 +1,17 @@
+package day7.Task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+        Player player5 = new Player();
+        Player player6 = new Player();
+        Player.info();
+        Player.counter();
+        Player.info();
+        Player.run();
+        Player.info();
+    }
+}
