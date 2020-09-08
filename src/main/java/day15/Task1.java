78f8a700af3313f9bf31046e5d1d03999e16d91d
nickart98 <nikita.artemov.9870@gmail.com>
2020-09-09 03:53:56 +0700

    Added solution for day15

---
+++ src/main/java/day15/Task1.java
@@ -0,0 +1,23 @@
+package day15;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("files/shoes.csv");
+        Scanner scanner = new Scanner(file);
+        File fileOut = new File("files/result.txt");
+        PrintWriter pw = new PrintWriter(fileOut);
+        while (scanner.hasNextLine()) {
+            String[] array = scanner.nextLine().split(";");
+            if (Integer.parseInt(array[2]) == 0) {
+                pw.println(array[0] + ", " + array[1] + ", " + array[2]);
+            }
+        }
+        pw.close();
+        scanner.close();
+    }
+}
